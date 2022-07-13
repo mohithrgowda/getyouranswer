98ef69ee914753358b84ddea6e487c68c0c7265d
Mohith S R <srmohith@plansource.com>
2022-07-13 13:28:34 +0530

    debuged notice appearing twice

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -25,7 +25,7 @@ class QuestionsController < ApplicationController
 
     respond_to do |format|
       if @question.save
-        format.html { redirect_to question_url(@question)}
+        format.html { redirect_to question_url(@question), notice: "Question was successfully created"}
         format.json { render :show, status: :created, location: @question }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class QuestionsController < ApplicationController
   def update
     respond_to do |format|
       if @question.update(question_params)
-        format.html { redirect_to question_url(@question)}
+        format.html { redirect_to question_url(@question), notice: "Question was successfully updated"}
         format.json { render :show, status: :ok, location: @question }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class QuestionsController < ApplicationController
     @question.destroy
 
     respond_to do |format|
-      format.html { redirect_to questions_url }
+      format.html { redirect_to questions_url ,notice: "Question was successfully destroyed" }
       format.json { head :no_content }
     end
   end
