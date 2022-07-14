b2fd503fbe1e47580fa260df03cd2aee8f8c373a
Mohith S R <srmohith@plansource.com>
2022-07-14 11:44:23 +0530

    questions order by desc and created user profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions' ,
     registrations: 'users/registrations'
   }
+  get "/u/:id" ,to: "users#profile" , as: "user"
  
   get "about" ,to: "pages#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
