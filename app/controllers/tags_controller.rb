fa3e68bc70b845cddf9d5a0c5ba11b587b56c7dc
Mohith S R <srmohith@plansource.com>
2022-07-18 15:30:25 +0530

    tag new ,index done

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,33 @@
 class TagsController < ApplicationController
   def new
+    @tag=Tag.new
+  end
+  def create
+    @tag=Tag.new(tag_params)
+    respond_to do |format|
+      if @tag.save
+        format.html { redirect_to tags_url(@tag), notice: "Tag was successfully created"}
+        format.json { render :show, status: :created, location: @tag }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @tag.errors, status: :unprocessable_entity }
+      end
+    end
+    
   end
 
   def index
+    @tags=Tag.paginate(page: params[:page], per_page: 5).order(created_at: :desc)
+    
   end
   
   def show
+    @tag=Tag.find(params[:id])
     
+  end
+
+  private
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 end
